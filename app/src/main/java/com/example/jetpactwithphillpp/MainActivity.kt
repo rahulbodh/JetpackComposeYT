daee813daedf9ee984db57b46d972e7a7323cadc
Rahul <rahulbodh819@gmail.com>
2025-04-28 12:27:46 +0530

    text styling - typography and shadow

--- app/src/main/java/com/example/jetpactwithphillpp/MainActivity.kt
+++ app/src/main/java/com/example/jetpactwithphillpp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,13 +23,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpactwithphillpp.ui.theme.JetpactWithPhillppTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,6 +56,56 @@ class MainActivity : ComponentActivity() {
             }
 
         }
+    }
+}
+
+@Composable
+fun TextStyling( modifier: Modifier = Modifier) {
+    Column ( modifier = modifier
+        .fillMaxSize()
+        .background(Color.Cyan)){
+        Text(
+            text = "Jetpack Compose",
+            lineHeight = 30.sp,
+            fontSize = 30.sp,
+            letterSpacing = 2.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .shadow(elevation = 2.dp, shape = RoundedCornerShape(8.dp))
+        )
+
+        Text(
+            text = "Headline Text",
+            fontSize = 30.sp,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = Color.Black.copy(alpha = 0.5f),
+                    offset = Offset(4f ,4f),
+                    blurRadius = 8f
+                )
+            ),
+            modifier = Modifier.padding(16.dp)
+        )
+        
+        Text(
+            text = "Underline and Striketroough text",
+            style = MaterialTheme.typography.bodyMedium,
+            fontSize = 20.sp,
+            textDecoration = TextDecoration.combine(
+                listOf(
+                    TextDecoration.Underline,
+                    TextDecoration.LineThrough
+                )
+            ),
+            modifier = Modifier.padding(16.dp)
+        )
+
     }
 }
 
@@ -100,13 +160,19 @@ fun ImageCard(
     }
 }
 
+//@Preview(showBackground = true)
+//@Composable
+//fun ImagePreview() {
+//    val painter = painterResource(id = R.drawable.iron_man)
+//    ImageCard(
+//        painter = painter,
+//        contentDescription = "Image is pretty cool",
+//        title = "Image is pretty cool"
+//    )
+//}
+
 @Preview(showBackground = true)
 @Composable
-fun ImagePreview() {
-    val painter = painterResource(id = R.drawable.iron_man)
-    ImageCard(
-        painter = painter,
-        contentDescription = "Image is pretty cool",
-        title = "Image is pretty cool"
-    )
+fun FunctionPreview() {
+    TextStyling()
 }
