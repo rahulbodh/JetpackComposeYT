99e37f103aab7044cf70c1274f57fab5a14cf459
Rahul <rahulbodh819@gmail.com>
2025-05-08 12:06:33 +0530

    make list scrollable

--- app/src/main/java/com/example/jetpactwithphillpp/MainActivity.kt
+++ app/src/main/java/com/example/jetpactwithphillpp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,8 +18,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -80,7 +83,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ListExample(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
         for (i in 1..30) {
             Text(
                 text = "Item $i",
